fbec9b86665eaa28aa8ac79c8822ab48c9e54506
tiff-wang <wangtiffany@gmail.com>
2018-09-15 16:28:12 -0400

    complete the majority of the ui

--- static/js/server.js
+++ static/js/server.js
@@ -1,11 +1,13 @@
 function getData() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           // Typical action to be performed when the document is ready:
-           console.log(xhttp.responseText);
-        }
-    };
-    xhttp.open("POST", "/tests/endpoint", true);
-    xhttp.send();
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //        // Typical action to be performed when the document is ready:
+    //         
+    //     }
+    // };
+    // xhttp.open("POST", "/tests/endpoint", true);
+    // xhttp.send();
+    var url = 'https://www.rover.com/blog/wp-content/uploads/2017/08/puppy-header-960x540.jpg';
+    document.getElementById('query-result').style.backgroundImage = "url(" + url + ")";
 }
