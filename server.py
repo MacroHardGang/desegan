81e712a330d9146aed6308cd87d08b3db3c8aaa3
frank-ye <frank.jing.ye@gmail.com>
2018-09-15 12:21:27 -0400

    cleaned code

--- server.py
+++ server.py
@@ -9,21 +9,22 @@ test_filename = 'tiff_yhack.png'
 
 
 # Handles POST request from client
-@app.route('/test/queryModel', methods=['POST'])
+
+@app.route('/tests/queryModel', methods=['POST']) 
+
 def my_test_endpoint():
     input_json = request.get_json(force=True)
-    
-    # Extract string to feed into ML model
-    descr_inst = input_json.get('descriptions')[0].get('text')
+    descr_inst = input_json.get('descriptions')
     print ('Client input:', descr_inst)
     
     ## SEND TO MODEL
 
     status = {'Data received': 'Success'}
     return jsonify(status)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'Hello Hansen, if you see this you went to address localhost:5000/'
 
 if __name__ == '__main__':
     app.run(debug=True)
