971abfa29b99905f125c5d1cff3d6c8fac786064
John Wu <john.wu@mail.mcgill.ca>
2018-09-15 16:32:37 -0400

    Fixed endpoint

--- application.py
+++ application.py
@@ -1,30 +1,23 @@
 # Simple server to handle post request from client 
 
 from flask import Flask, render_template, request, url_for, jsonify, send_file
+from run import generateCriminal
+
 app = Flask(__name__)
 
-# Test purposes
-test_filepath = './test_data/'
-test_filename = 'tiff_yhack.png'
-
-
 # Handles POST request from client
-
-@app.route('/tests/queryModel', methods=['POST']) 
-
-def my_test_endpoint():
-    input_json = request.get_json(force=True)
-    descr_inst = input_json.get('descriptions')
-    print ('Client input:', descr_inst)
-    
-    ## SEND TO MODEL
-
-    status = {'Data received': 'Success'}
-    return jsonify(status)
+@app.route('/generate-image', methods=['POST']) 
+def generateImage():
+    description = request.form['description']
+    print ('Client input:', description)
+    # Send to model
+    generated_image_url = generateCriminal(description)
+    result = {'image_url': generated_image_url}
+    return jsonify(result)
 
 @app.route('/')
 def index():
-    return 'Hello Hansen, if you see this you went to address localhost:5000/'
+    return 'Test passed.'
 
 if __name__ == '__main__':
     app.run(debug=True)
