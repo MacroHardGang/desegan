f283614232cc8cd1a96530a47f191eaccc2e625e
John Wu <john.wu@mail.mcgill.ca>
2018-09-15 06:22:16 -0400

    Finished uploading data

--- data-generation/data-gen.py
+++ data-generation/data-gen.py
@@ -1,10 +1,12 @@
 import requests
 import json
+import random
 
 # Microsoft Face API Key
 SUBSCRIPTION_KEY = "d344c023fa454be4b04928925c3d0d59"
 FACE_API_URL = "https://eastus.api.cognitive.microsoft.com/face/v1.0/detect"
 
+''' Retrieve facial descriptions using Microsoft Face API'''
 def retrieveFaceDesc(image_url):
     headers = {"Ocp-Apim-Subscription-Key": SUBSCRIPTION_KEY}
     params = {
@@ -18,13 +20,12 @@ def retrieveFaceDesc(image_url):
     faces = response.json()
     return faces[0]
 
+''' Create description strings '''
 def createDescString(description):
     # Gender
     gender = description["faceAttributes"]["gender"] + " "
-
     # Age
     age = str(int(description["faceAttributes"]["age"])) + " years old "
-
     # Facial hair
     facial = "" 
     if (description["faceAttributes"]["facialHair"]["moustache"] > 0.3):
@@ -33,12 +34,10 @@ def createDescString(description):
         facial = facial + "beard "
     if (description["faceAttributes"]["facialHair"]["sideburns"] > 0.3):
         facial = facial + "sideburns "
-
     # Glasses
     glasses = ""
     if (description["faceAttributes"]["glasses"] != "NoGlasses"):
         glasses = glasses + "glasses "
-
     # Hair
     hair = ""
     if (description["faceAttributes"]["hair"]["invisible"]):
@@ -51,13 +50,32 @@ def createDescString(description):
                 color = x["color"]
                 highest_confidence = x["confidence"] 
         hair = hair + color + " hair"
-    
     # Create string
     description_string = gender + age + facial + glasses + hair
-    print(description_string)
+    return description_string
 
-desc = retrieveFaceDesc("https://htn.blob.core.windows.net/htn-blob/13234.jpg")
-createDescString(desc)
+image_id = 401
+data = []
+for n in range(1,4):
+    image_url = "https://htn.blob.core.windows.net/htn-blob/" + str(n) + ".jpg"
+    msft_desc = retrieveFaceDesc(image_url)
+    desc_string = createDescString(msft_desc)
+    desc = {
+            "img_id": image_id,
+            "image": image_url,
+            "descriptions": [
+                {
+                    "desc_id": random.randint(1,10000000000),
+                    "text": desc_string
+                }
+            ]
+        }
+    data.append(desc)
+    image_id = image_id + 1
+    print("Image #" + str(n) + ": " + desc_string)
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
 
 
 
--- data-generation/data-upload.py
+++ data-generation/data-upload.py
@@ -7,21 +7,15 @@ ACCOUNT_NAME = 'htn'
 ACCOUNT_KEY = 'Wa6X+0mdj74FBEPczVimdRzj6Z7KDsUHKPckyP0UdGWpZRjbJuERLR8T/JJIpNBOxZA+Q+5aSmx1xYM/HmTLqQ=='
 CONTAINER_NAME = 'htn-blob'
 
-try:
-    # Create the BlockBlockService that is used to call the Blob service for the storage account
-    block_blob_service = BlockBlobService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)
+def uploadImage(n):
+    try:
+        # Create the BlockBlockService that is used to call the Blob service for the storage account
+        block_blob_service = BlockBlobService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)
+        # Upload file
+        print("Uploading Image #" + str(n))
+        block_blob_service.create_blob_from_path(CONTAINER_NAME, str(n) + '.jpg', 'images/'+ str(n) +'.jpg')
+    except Exception as e:
+        print(e)
 
-    # Upload file
-    block_blob_service.create_blob_from_path(CONTAINER_NAME, '1.jpg', 'images/1.jpg')
-
-except Exception as e:
-    print(e)
-
-
-# n = 1
-# for subdir, dirs, files in os.walk(rootdir):
-#     for file in files:
-#         f = os.path.join(subdir, file)
-#         os.rename(f, "image-new/"+ str(n) +".jpg")
-#         n = n + 1
-#         print(f)
+for n in range(1,13248):
+    uploadImage(n)
